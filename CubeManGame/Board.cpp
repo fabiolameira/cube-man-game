c3635545851df96dcb43e88abd2b4f9631a1cd81
leonardocosta197 <leonardocosta197@gmail.com>
2020-01-11 00:27:34 +0000

    add map 1

--- CubeManGame/Board.cpp
+++ CubeManGame/Board.cpp
@@ -18,15 +18,67 @@ void Board::map() {
 			this->matrix[x][y] = true;
 		}
     }
-	this->matrix[1][4] = false;
-	this->matrix[1][5] = false;
-	this->matrix[1][6] = false;
-	this->matrix[2][4] = false;
+	//0
+	this->matrix[0][10] = false;
+	this->matrix[0][11] = false;
+	this->matrix[0][8] = false;
+	this->matrix[0][7] = false;
+	this->matrix[0][5] = false;
+	this->matrix[0][4] = false;
+	this->matrix[0][3] = false;
+	this->matrix[0][2] = false;
+	this->matrix[0][1] = false;
+	this->matrix[0][0] = false;
+	//1
+	this->matrix[1][11] = false;
+	this->matrix[1][0] = false;
+	//2
+	this->matrix[2][9] = false;
+	this->matrix[2][8] = false;
+	this->matrix[2][0] = false;
+
+	//3
+	this->matrix[3][9] = false;
+	this->matrix[3][8] = false;
 	this->matrix[3][5] = false;
-	this->matrix[3][6] = false;
-	this->matrix[4][4] = false;
+	this->matrix[3][4] = false;
+	this->matrix[3][3] = false;
+	this->matrix[3][2] = false;
+	this->matrix[3][1] = false;
+	this->matrix[3][0] = false;
+	//4
+	this->matrix[4][11] = false;
 	this->matrix[4][5] = false;
-	this->matrix[4][6] = false;
+	this->matrix[4][3] = false;
+	//5
+	this->matrix[5][11] = false;
+	this->matrix[5][10] = false;
+	this->matrix[5][5] = false;
+	//6
+	this->matrix[6][2] = false;
+	//7
+	this->matrix[7][2] = false;
+	this->matrix[7][3] = false;
+	//8
+	this->matrix[8][11] = false;
+	this->matrix[8][10] = false;
+	this->matrix[8][9] = false;
+	this->matrix[8][8] = false;
+	this->matrix[8][2] = false;
+	//10
+	this->matrix[9][8] = false;
+	this->matrix[9][5] = false;
+	this->matrix[9][4] = false;
+	//11
+	this->matrix[10][1] = false;
+	this->matrix[10][5] = false;
+	this->matrix[10][4] = false;
+	this->matrix[10][11] = false;
+	//12
+	this->matrix[11][1] = false;
+	this->matrix[11][2] = false;
+	this->matrix[11][11] = false;
+	this->matrix[11][10] = false;
 }
 
 void Board::draw() {
